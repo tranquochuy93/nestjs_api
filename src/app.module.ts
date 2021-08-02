7836adf67953b5f447ec47e2e24c8fea563a2e0e
tranquochuy93 <tranquochuyqn93@gmail.com>
2021-08-02 12:26:31 +0700

    add new project

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+
+@Module({
+  imports: [
+    ProductsModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://HuyTQ:18021993@cluster0.ukjsc.mongodb.net/nestjs-demo?retryWrites=true&w=majority',
+    ),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
